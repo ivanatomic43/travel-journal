5e172bbdeafb6f645b26e91304b847912fb5e383
ivanatomic43 <ivana.tomic96@gmail.com>
2022-11-25 23:49:57 +0100

    Fix margins & code

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -4,20 +4,19 @@ import {ImLocation} from 'react-icons/im'
 
 const TravelCard = () => {
 
-    const renderCards = data.map(function(card){
+    const renderCards = data.map(function(card) {
         return(
             <section key={card.id}>
                 <img className="card-img" src={card.imageUrl} />
 
                 <div className="card--div">
                     <div className="card-location">
-                        <ImLocation className="card-icon"/>    
-                        <h4 className="card-country">{card.location.toUpperCase()}</h4>
+                        <ImLocation className="card-icon"/>
+                        <h4 className="card-country"> {card.location.toUpperCase()} </h4>
                         <a href={card.googleMapsUrl} className="card-google-maps-link">View on Google Maps</a>
                     </div>
-                    <h1 className="card-location-title">{card.title}</h1>
+                    <h1 className="card-location-title"> {card.title} </h1>
                     <div className="card-details">
-                        
                         <p className="card-date"> {card.startDate} - {card.endDate} </p>
                         <p className="card-description"> {card.description} </p>
                     </div>
@@ -25,6 +24,7 @@ const TravelCard = () => {
             </section>
         );
     });
+
     return(
         <div>{renderCards}</div>
     );
